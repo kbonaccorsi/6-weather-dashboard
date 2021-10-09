647ea7100c8b0cfd2ccd40365be6f1e371c958ab
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-10-09 15:14:23 -0500

    saves searches to local storage, and adds them to the previous searches list

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,12 +1,19 @@
 //Global Variables- API keys
 const apiKeyFuture = "2333826092b58c3b0f9ed3b12d33f616";
 const apiKeyCurrent = "6253c56c808bcdaa72af78a1a5171dde";
+//let currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKeyCurrent;
+//let futureQueryURL = "" + city + "&appid=" + apiKeyFuture;
 
 // Global Variables
+const searchButton = document.getElementById("searchButton");
+const searchBoxEl = document.getElementById("searchBox");
+const searchListEl = document.getElementById("searchHistoryListEl");
 const currentDateEl = document.getElementById("currentDate");
 const currentTimeEl = document.getElementById("currentTime");
 const today = moment();
-
+// let city;
+// let state;
+// let country;
 
 
 //sets the current date and time
@@ -14,15 +21,21 @@ currentDateEl.textContent = today.format("dddd, MMMM, DD, YYYY");
 currentTimeEl.textContent = today.format("h:mm a");
 
 
-/*fetch information from weather API
-fetch(insert requested weather API URL)
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data) {
-        return data.json()
-    });
-*/
+// fetch (currentQueryURL)
+//     .then(function(response) {
+//         return response.json()
+//     })
+//     .then(function(data) {
+//         return data.json()
+//     });
+
+//     fetch (futureQueryURL)
+//     .then(function(response) {
+// return response.json()
+//     })
+//     .then(function(data) {
+//         return data.json()
+//     });
 
 
 /*
@@ -36,22 +49,57 @@ add.class "severe"
 */
 
 
-/* saves searches to local storage when search button is clicked, or enter (key13) is pressed
-searchSave.function() {
-searchButton.getEventListener("click", function()) {
-    event.preventDefault()
-localStorage.getItem.json.parse(changes object to string)
-}
+let searches =[];
+
+//user inputs their search, and the search is added as a new list item to the list of previous searches
+function renderSearches() {
+    searchListEl.innerHTML ="";
+
+    for (i = 0; i < searches.length; i++) {
+        let search = searches[i];
+
+    let li = document.createElement("li");
+    li.textContent = (search);
+    li.setAttribute("data-index", i);
+
+    searchListEl.appendChild(li);
+    };
 };
-*/
 
+// saves searches to local storage when search button is clicked, or enter (key13) is pressed
+function init() {
+    let searchSave = JSON.parse(localStorage.getItem(searchBoxEl));
+    if(searchSave !== null) {
+    searches  = searchSave;
+    };
+    renderSearches()
+};
 
+//user searches a city and that search is saved in local storage
+function storeSearches() {
+    localStorage.setItem(searchBoxEl, JSON.stringify(searches))
+};
 
-
-/*saves searches to .searchHistoryList, from local storage
-searchHistory.function() {
-localStorage.setItem.json.setstringify (changes string to object)
-(.searchHistoryList).add(li) for each search submitted
-return ()
+//make the search button, and enter submit the search
+searchBoxEl.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
 }
-*/
+});
+
+searchButton.addEventListener("click", function() {
+    const searchText = (searchBoxEl.value.trim());
+    
+    if (searchText === "") {
+        return;
+    };
+
+
+    searches.push(searchText);
+    searchBoxEl.value = "";
+
+    storeSearches();
+    renderSearches();
+});
+
+init()
