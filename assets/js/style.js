5368d8fcf1c3d3b31a19cc6b58e07ed0ae9b3ffc
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-10-10 12:57:23 -0500

    layout is how I want it, fetch is working, need to work on saving searches and pulling information from fetch data

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,10 +1,5 @@
-//Global Variables- API keys
-//const apiKeyFuture = "2333826092b58c3b0f9ed3b12d33f616";
-// let city is giving a 404 message when being used instead of typing the city name straight into the API request
+//Global Variables
 const apiKeyCurrent = "6253c56c808bcdaa72af78a1a5171dde";
-
-
-// Global Variables
 const searchButton = document.getElementById("searchButton");
 const searchBoxEl = document.getElementById("searchBox");
 const searchListEl = document.getElementById("searchHistoryListEl");
@@ -12,87 +7,73 @@ const currentDateEl = document.getElementById("currentDate");
 const currentTimeEl = document.getElementById("currentTime");
 const today = moment();
 
-
-
 //sets the current date and time
 currentDateEl.textContent = today.format("dddd, MMMM, DD, YYYY");
 currentTimeEl.textContent = today.format("h:mm a");
 
 
-/*
-let uvIndexCheck function() {
-if UV index is === favorable
-add.class "favorable"
-else if UV index is === moderate
-add.class "moderate"
-else if UV index is === severe
-add.class "severe"
-*/
-
-
+//searches is put into an array
 let searches = [];
 
 //user inputs their search, and the search is added as a new list item to the list of previous searches
+function getCurrentWeather() {
+    // city is made up of user input in the search box
+    let city = searchBoxEl.value.trim();
+    console.log(city);
 
-function getAPI() {
-
-    let city = searchBoxEl.value;
-    // let state;
-    // let country;
-    
+    //API request URL
     let currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKeyCurrent;
     console.log(currentQueryURL);
-    //let futureQueryURL = "" + city + "&appid=" + apiKeyFuture;
 
-
+    // me reaching out to openweathermap.org to get information about the city (user determined)
     fetch(currentQueryURL)
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
             console.log(data)
-
-            searchListEl.textContent = "";
-
-            for (i = 0; i < searches.length; i++) {
-                let search = searches[i];
-
-                let a = document.createElement("a");
-                let li = document.createElement("li");
-
-                li.textContent = (search);
-                li.setAttribute("city", i);
-                a.href = (currentQueryURL);
-
-                searchListEl.appendChild(a);
-                a.appendChild(li);
-            };
         });
 };
 
-//     fetch (futureQueryURL)
-//     .then(function(response) {
-// return response.json()
-//     })
-//     .then(function(data) {
-//         return data.json()
-//     });
+
+
+//     function conductSearch() {
+
+//             //     searchListEl.textContent = "";
+
+//         //     for (i = 0; i < searches.length; i++) {
+//         //         let search = searches[i];
+
+//         //         let a = document.createElement("a");
+//         //         let li = document.createElement("li");
+
+//         //         li.textContent = (search);
+//         //         li.setAttribute("data-index", i);
+//         //         a.href = (currentQueryURL);
+
+//         //         searchListEl.appendChild(a);
+//         //         a.appendChild(li);
+//         //     };
+//     };    
+
+
+
+// // saves searches to local storage when search button is clicked, or enter (key13) is pressed
+// function saveSearches() {
+//     let searchSave = JSON.parse(localStorage.getItem(searchBoxEl));
+//     if (searchSave !== null) {
+//         searches = searchSave;
+//     };
+// };
+
+// //user searches a city and that search is saved in local storage
+// function storeSearches() {
+//     localStorage.setItem(searchBoxEl, JSON.stringify(searches))
+// };
 
 
 
 
-// saves searches to local storage when search button is clicked, or enter (key13) is pressed
-function init() {
-    let searchSave = JSON.parse(localStorage.getItem(searchBoxEl));
-    if (searchSave !== null) {
-        searches = searchSave;
-    };
-};
-
-//user searches a city and that search is saved in local storage
-function storeSearches() {
-    localStorage.setItem(searchBoxEl, JSON.stringify(searches))
-};
 
 //make the search button, and enter submit the search
 searchBoxEl.addEventListener("keyup", function (event) {
@@ -106,17 +87,47 @@ searchButton.addEventListener("click", function (event) {
     const searchText = (searchBoxEl.value.trim());
 
     if (searchText === "") {
+        alert("Please enter a valid city name");
         return;
     };
-
+    //searches array gets searchText added to thend of the searches array and returns the new length of the array
     searches.push(searchText);
-    
 
-    storeSearches()
-    getAPI()
+    getCurrentWeather()
+    //storeSearches()
 
     // clearing the search box to prepare for another search
     searchBoxEl.value = "";
 });
 
-init()
+//saveSearches()
+
+
+/*
+let uvIndexCheck function() {
+if UV index is === favorable
+add.class "favorable"
+else if UV index is === moderate
+add.class "moderate"
+else if UV index is === severe
+add.class "severe"
+*/
+
+/*
+let conditions function() {
+    if condtion
+}
+*/
+
+//const apiKeyFuture = "2333826092b58c3b0f9ed3b12d33f616";
+// let state;
+// let country;
+//let futureQueryURL = "" + city + "&appid=" + apiKeyFuture;
+
+//     fetch (futureQueryURL)
+//     .then(function(response) {
+// return response.json()
+//     })
+//     .then(function(data) {
+//         return data.json()
+//     });
